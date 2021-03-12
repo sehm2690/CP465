8a770166b7785ab6dadd89c41ba81b4f343fd805
Wilson <wilson.vuu99@gmail.com>
2021-03-12 18:24:28 -0500

    commit phase 1

---
+++ watchlist.php
@@ -0,0 +1,295 @@
+<?php
+    include_once'header.php'
+?>
+
+<main>
+<link rel='stylesheet' href='watchlist.scss' /> 
+<head>
+<!-- 
+<script src=
+;(function($) { 
+        $.fn.fixMe = function() {
+        return this.each(function() {
+            var $this = $(this),
+                $t_fixed;
+            function init() {
+                $this.wrap('<div class="container" />');
+                $t_fixed = $this.clone();
+                $t_fixed.find("tbody").remove().end().addClass("fixed").insertBefore($this);
+                resizeFixed();
+            }
+            function resizeFixed() {
+                $t_fixed.find("th").each(function(index) {
+                    $(this).css("width",$this.find("th").eq(index).outerWidth()+"px");
+                });
+            }
+            function scrollFixed() {
+                var offset = $(this).scrollTop(),
+                tableOffsetTop = $this.offset().top,
+                tableOffsetBottom = tableOffsetTop + $this.height() - $this.find("thead").height();
+                if(offset < tableOffsetTop || offset > tableOffsetBottom)
+                    $t_fixed.hide();
+                else if(offset >= tableOffsetTop && offset <= tableOffsetBottom && $t_fixed.is(":hidden"))
+                    $t_fixed.show();
+            }
+            $(window).resize(resizeFixed);
+            $(window).scroll(scrollFixed);
+            init();
+        });
+        };
+    })(jQuery);
+    
+    $(document).ready(function(){
+        $("table").fixMe();
+        $(".up").click(function() {
+        $('html, body').animate({
+        scrollTop: 0
+        }, 2000);
+    });
+    });
+></script> -->
+
+ 
+</head>
+
+    
+<!-- <h1>&darr; SCROLL &darr;</h1> -->
+<table class="blue">
+  <thead>
+    <tr>
+      <th>Stock Name</th>
+      <th>Last Price</th>
+      <th>Change</th>
+      <th>% Change</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    <tr>
+      <td>DBX</td>
+      <td>27.06</td>
+      <td>1.61</td>
+      <td>6.33</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+<!-- <h1 class="scrollMore">&darr; SCROLL MORE &darr;</h1> -->
+<table class="purple">
+  <thead>
+    <tr>
+      <th>Stock Name</th>
+      <th>Last Price</th>
+      <th>Change</th>
+      <th>% Change</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+    <tr>
+      <td>JWN</td>
+      <td>41.37</td>
+      <td>3.97</td>
+      <td>10.61</td>
+    </tr>
+  </tbody>
+</table>
+<!-- <h1 class="up scrollMore">&uarr; UP &uarr;</h1> -->
+
+       
+</main>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="watchlist.js"></script>
+
+<?php
+    include_once'footer.php'
+?>
